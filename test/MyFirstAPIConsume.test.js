423f1587168f944345298e258728acb22f877422
Eduardo José Maya Rodriguez <36246464+edopore@users.noreply.github.com>
2022-06-28 19:24:41 -0500

    First api test (#4)
    * setup mocha configuration
    
    * This is a Workshop about Api Testing in JavaScript
    
    * First API consume test
    
    * conflict with package.js resolved
    
    * HelloWorld.js deleted
    
    * Conflict package-lock.json resolved
    
    * packae-lock.json conflicts resolved again
    
    * Update test/MyFirstAPIConsume.test.js
    
    Co-authored-by: Daniel Galvis <34688953+danielgalvis98@users.noreply.github.com>
    
    * Update test/MyFirstAPIConsume.test.js
    
    Co-authored-by: Daniel Galvis <34688953+danielgalvis98@users.noreply.github.com>
    
    Co-authored-by: Daniel Galvis <34688953+danielgalvis98@users.noreply.github.com>

---
+++ test/MyFirstAPIConsume.test.js
@@ -0,0 +1,62 @@
+const axios = require('axios');
+const { expect } = require('chai');
+const { StatusCodes } = require('http-status-codes');
+describe('First Api Tests', () => {
+    it('Consume GET Service', async () => {
+        const response = await axios.get('https://httpbin.org/ip');
+        expect(response.status).to.equal(StatusCodes.OK);
+        expect(response.data).to.have.property('origin');
+    });
+
+    it('Consume GET Service with query parameters', async () => {
+        const query = {
+            name: 'John',
+            age: '31',
+            city: 'New York'
+        };
+
+        const response = await axios.get('https://httpbin.org/get', { query });
+        expect(response.status).to.equal(StatusCodes.OK);
+        expect(response.config.query).to.eql(query);
+    });
+
+    it('Consume HEAD Service', async () => {
+        const response = await axios.head('https://httpbin.org/');
+        expect(response.status).to.equal(StatusCodes.OK);
+    });
+
+    it('Consume PUT Service', async () => {
+        const dataput = {
+            name : 'Eduardo',
+            age : '28',
+            city : 'Mocoa'
+        }
+        const response = await axios.put('https://httpbin.org/put', dataput );
+        expect(response.status).to.equal(StatusCodes.OK);
+        expect(response.data.json.dataput).to.eql(dataput)
+    });
+
+    it('Consume PATCH Service', async () => {
+        const datapatch = {
+            id : 12,
+            ip : '192.168.5.55',
+            connected : 'false',
+            username : 'figaro'
+        }
+        const response = await axios.patch('https://httpbin.org/patch', datapatch);
+        expect(response.status).to.equal(StatusCodes.OK);
+        expect(response.data.json.datapatch.id).to.eql(datapatch.id)
+    });
+
+    it('Consume DELETE Service', async () => {
+        const query = {
+            id : 15
+        }
+        const response = await axios.delete('https://httpbin.org/delete',{query});
+        expect(response.status).to.equal(StatusCodes.OK);
+        expect(response.data.data).to.equal("")
+        
+    });
+
+
+});
