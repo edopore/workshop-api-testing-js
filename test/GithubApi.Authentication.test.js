bd9efbe39e00ffc398b62b3a499bbd772b841c16
edopore <ediij1994@gmail.com>
2022-07-04 14:52:32 -0500

    GitHubApi.Authentication.test.js file modified

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -3,7 +3,7 @@ const { expect } = require('chai');
 const axios = require('axios');
 
 const urlBase = 'https://api.github.com';
-const githubUserName = 'edopore';
+const githubUserName = 'AgileTestingColombia';
 const repository = 'workshop-api-testing-js';
 
 describe('Github Api Test', () => {
@@ -19,7 +19,11 @@ describe('Github Api Test', () => {
       expect(response.data.description).equal('This is a Workshop about Api Testing in JavaScript');
     });
     it('Via OAuth2 Tokens by parameter', async () => {
-      const response = await axios.get(`${urlBase}/repos/${githubUserName}/${repository}`, { access_token: process.env.ACCESS_TOKEN });
+      const response = await axios.get(
+        `${urlBase}/repos/${githubUserName}/${repository}`,
+        { access_token: process.env.ACCESS_TOKEN }
+      );
+
       expect(response.status).to.equal(StatusCodes.OK);
       expect(response.data.description).equal('This is a Workshop about Api Testing in JavaScript');
     });
