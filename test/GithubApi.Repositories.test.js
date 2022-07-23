d4a9691a2e13ed07bca3ca09472ff77a69381acb
edopore <ediij1994@gmail.com>
2022-07-23 18:17:49 -0500

    GithubApi.Repositories.test.js file reformatted

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -3,6 +3,7 @@ const { expect } = require('chai');
 const axios = require('axios');
 const chai = require('chai');
 const chaiSubset = require('chai-subset');
+const md5 = require('md5');
 
 chai.use(chaiSubset);
 
@@ -14,38 +15,53 @@ const repoName = 'jasmine-json-report';
 const desc = 'A Simple Jasmine JSON Report';
 
 describe('Github Api Test', () => {
-  it('GET method consumption', async () => {
-    const response = await axios.get(`${urlBase}/users/${githubUserName}`);
-    expect(response.status).to.equal(StatusCodes.OK);
-    expect(response.data.login).to.equal(githubUserName);
-    expect(response.data.company).to.equal(company);
-    expect(response.data.name).to.equal(name);
-  });
-  it('GET method for repos', async () => {
-    const repos = await axios.get(`${urlBase}/users/${githubUserName}/repos`);
-    const repository = repos.data.find((element) => element.name === repoName);
-    expect(repos.status).to.equal(StatusCodes.OK);
-    /* Expect name of repository jasmine-json-report */
-    expect(repository.name).to.equal(repoName);
-    expect(repository.private).to.equal(false);
-    expect(repository.description).to.equal(desc);
+  let userResponse;
+
+  before(async () => {
+    userResponse = await axios.get(`${urlBase}/users/${githubUserName}`);
   });
 
-  it('GET zip file repository', async () => {
-    /* Download zip file repo https://api.github.com/repos/aperdomob/jasmine-json-report/zipball/master */
-    const repos = await axios.get(`${urlBase}/repos/${githubUserName}/${repoName}/zipball/master`);
-    expect(repos.status).to.equal(StatusCodes.OK);
+  it('GET method consumption', () => {
+    expect(userResponse.status).to.equal(StatusCodes.OK);
+    expect(userResponse.data.login).to.equal(githubUserName);
+    expect(userResponse.data.company).to.equal(company);
+    expect(userResponse.data.name).to.equal(name);
   });
-  it('GET repos content', async () => {
-    const repoContent = await axios.get(`${urlBase}/repos/${githubUserName}/${repoName}/contents`);
-    expect(repoContent.status).to.equal(StatusCodes.OK);
-    expect(repoContent.data).to.containSubset([{ name: 'README.md' }]);
-    expect(repoContent.data).to.containSubset([{ path: 'README.md' }]);
-    expect(repoContent.data).to.containSubset([{ sha: '360eee6c223cee31e2a59632a2bb9e710a52cdc0' }]);
-  });
-  it('GET README.md file from repository', async () => {
-    /* Download README.md file repo https://raw.githubusercontent.com/aperdomob/jasmine-json-report/master/README.md */
-    const fileContent = await axios.get(`https://raw.githubusercontent.com/${githubUserName}/${repoName}/master/README.md`);
-    expect(fileContent.status).to.equal(StatusCodes.OK);
+  describe('Hypermedia', () => {
+    let repository;
+    before(async () => {
+      const repositoryRespose = await axios.get(`${userResponse.data.repos_url}`);
+      repository = repositoryRespose.data.find((Repo) => Repo.name === repoName);
+    });
+    it('GET method for repos', () => {
+      expect(repository.name).to.equal(repoName);
+      expect(repository.private).to.equal(false);
+      expect(repository.description).to.equal(desc);
+    });
+    it('Downloading by zip', async () => {
+      // code to verify the zip download
+      const fileDownload = await axios.get(`${repository.url}/zipball/master`);
+      expect(fileDownload.status).to.equal(StatusCodes.OK);
+      expect(fileDownload.headers).to.contain({ 'content-type': 'application/zip' });
+    });
+    describe('GET repos content', () => {
+      // code to verify the readme and to download and verify the md5
+      let fileDownload;
+      before(async () => {
+        fileDownload = await axios.get(`${repository.url}/contents`);
+      });
+      it('GET README.md file info', () => {
+        expect(fileDownload.data).to.containSubset([{
+          name: 'README.md',
+          path: 'README.md',
+          sha: '360eee6c223cee31e2a59632a2bb9e710a52cdc0'
+        }]);
+      });
+      it('Download README.md file and check md5', async () => {
+        const fileContent = await axios.get(`https://raw.githubusercontent.com/${githubUserName}/${repoName}/master/README.md`);
+        expect(fileContent.status).to.equal(StatusCodes.OK);
+        expect(md5(fileContent.data)).to.equal('497eb689648cbbda472b16baaee45731');
+      });
+    });
   });
 });
